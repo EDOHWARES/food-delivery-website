0c522d5f7b3ed6169abd4ebd828fc9af00ae2727
edohwares <edohemmanuel4real@gmail.com>
2024-07-19 15:52:08 +0100

    homepage header

--- food-delivery/src/App.jsx
+++ food-delivery/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 
 const App = () => {
   return (
     <div className='app'>
       <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+      </Routes>
     </div>
   )
 }
---
+++ food-delivery/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h1>Order your favourite food here.</h1>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients  and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
